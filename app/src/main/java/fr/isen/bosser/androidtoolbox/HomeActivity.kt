512f83db2a65e62d8f95415b3bae9d9f1928c758
bastienbosser <bastien.bosser@yahoo.com>
2020-01-20 16:00:33 +0100

    home page to cycle page ok

--- app/src/main/java/fr/isen/bosser/androidtoolbox/HomeActivity.kt
+++ app/src/main/java/fr/isen/bosser/androidtoolbox/HomeActivity.kt
@@ -1,12 +1,18 @@
 package fr.isen.bosser.androidtoolbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        cycle.setOnClickListener {
+            startActivity( Intent ( this,CycleDeVie::class.java))
+        }
     }
 }
