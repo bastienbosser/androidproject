e05ca2debb05d1ac2cce97d96543fdfa7ea76f93
bastienbosser <bastien.bosser@yahoo.com>
2020-01-20 15:21:20 +0100

    login page to home page ok

--- app/src/main/java/fr/isen/bosser/androidtoolbox/LoginActivity.kt
+++ app/src/main/java/fr/isen/bosser/androidtoolbox/LoginActivity.kt
@@ -1,5 +1,6 @@
 package fr.isen.bosser.androidtoolbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -13,9 +14,14 @@ class LoginActivity : AppCompatActivity() {
 
         validate.setOnClickListener {
             val login = login.text.toString()
-            val message = "tu as cliqué $login"
-            Toast.makeText( this@LoginActivity, message, Toast.LENGTH_LONG ).show()
+            val password = password.text.toString()
+            val message = "identifiant ou mot de passe invalide"
+            if (login=="admin" && password=="123") {
+                startActivity( Intent(this,HomeActivity::class.java) )
+            }
+            else {
+                Toast.makeText(this@LoginActivity, message, Toast.LENGTH_LONG).show()
+            }
         }
     }
-
 }
