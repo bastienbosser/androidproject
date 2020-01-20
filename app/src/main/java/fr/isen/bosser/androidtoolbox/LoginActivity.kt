6155584b1ded62506b7dd6294701d9fdaf7dcfa7
bastienbosser <bastien.bosser@yahoo.com>
2020-01-20 15:02:26 +0100

    function for print message ok

--- app/src/main/java/fr/isen/bosser/androidtoolbox/LoginActivity.kt
+++ app/src/main/java/fr/isen/bosser/androidtoolbox/LoginActivity.kt
@@ -2,11 +2,20 @@ package fr.isen.bosser.androidtoolbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        validate.setOnClickListener {
+            val login = login.text.toString()
+            val message = "tu as cliqué $login"
+            Toast.makeText( this@LoginActivity, message, Toast.LENGTH_LONG ).show()
+        }
     }
+
 }
