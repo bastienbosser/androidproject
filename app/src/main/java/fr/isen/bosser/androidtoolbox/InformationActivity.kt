82619ab6a4b334282e8ddb5310609e5d62791a1b
bastienbosser <bastien.bosser@yahoo.com>
2020-02-02 16:14:59 +0100

    option of camera ok

--- app/src/main/java/fr/isen/bosser/androidtoolbox/InformationActivity.kt
+++ app/src/main/java/fr/isen/bosser/androidtoolbox/InformationActivity.kt
@@ -3,11 +3,15 @@ package fr.isen.bosser.androidtoolbox
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_information.*
 
@@ -19,6 +23,33 @@ class InformationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_information)
 
         myimage.setOnClickListener{
+            optionImage()
+        }
+    }
+
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    private fun pickImageFromCamera() {
+        val intent1 = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent1.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent1, CAMERA_PICK_CODE)
+        }
+    }
+
+    private fun optionImage() {
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle("Change Image")
+
+        builder.setMessage("Choose between pick image in gallery or in camera")
+
+        builder.setPositiveButton("Gallery"){ _, _ ->
+            Toast.makeText(applicationContext,"go to gallery.",Toast.LENGTH_SHORT).show()
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
                 if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
                     PackageManager.PERMISSION_DENIED){
@@ -33,26 +64,52 @@ class InformationActivity : AppCompatActivity() {
                 pickImageFromGallery();
             }
         }
-    }
 
+        builder.setNegativeButton("Take Picture"){dialog,which ->
+            Toast.makeText(applicationContext,"go to camera",Toast.LENGTH_SHORT).show()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                    PackageManager.PERMISSION_DENIED){
+                    val permissions = arrayOf(Manifest.permission.CAMERA)
+                    requestPermissions(permissions, CAMERA_PICK_CODE)
+                }
+                else{
+                    pickImageFromCamera()
+                }
+            }
+            else{
+                pickImageFromCamera()
+            }
+        }
 
-    private fun pickImageFromGallery() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_PICK_CODE)
+        builder.setNeutralButton("Cancel"){_,_ ->
+            Toast.makeText(applicationContext,"cancel dialog.",Toast.LENGTH_SHORT).show()
+        }
+
+        builder.show()
     }
 
     companion object {
         private val IMAGE_PICK_CODE = 1000;
         private val PERMISSION_CODE = 1001;
+        private val CAMERA_PICK_CODE = 1002;
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         when(requestCode){
             PERMISSION_CODE -> {
-                if (grantResults.size >0 && grantResults[0] ==
+                if (grantResults.isNotEmpty() && grantResults[0] ==
                     PackageManager.PERMISSION_GRANTED){
                     pickImageFromGallery()
+                }
+                else{
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+            CAMERA_PICK_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                    pickImageFromCamera()
                 }
                 else{
                     Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
@@ -66,6 +123,9 @@ class InformationActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
             myimage.setImageURI(data?.data)
+        }
+        else if (resultCode == Activity.RESULT_OK && requestCode == CAMERA_PICK_CODE){
+            myimage.setImageBitmap(data?.extras?.get("data") as Bitmap)
         }
     }
 }
